2724c4df094aee863776d521d5dc15e06c57930b
Koyni <31552815+Koyni@users.noreply.github.com>
2018-04-02 09:45:30 -1000

    Add files via upload

---
+++ whatever.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def Scrape(url) :
+	with urllib.request.urlopen(url)
+	print (response.info())
+	html=response.read()
+	# do something
+
+Scrape('https://coinmarketcap.com/currencies/bitcoin/')
+
+import urllib.request
+with urllib.request.urlopen('https://coinmarketcap.com/currencies/bitcoin/') as response
+	html = response.read()
+
+def Webscrapper(url) :
+	
